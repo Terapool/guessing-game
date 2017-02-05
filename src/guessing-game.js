7dd43b47fe7a289680bcbc5aec21fe9f7e943d02
Sergey Dmitriev <terapool@gmail.com>
2017-02-05 20:51:52 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		
+		this.min = 0;
+		this.max = 0;
+		
+		}
 
-    setRange(min, max) {
-
+    setRange(min, max) 
+	{
+	this.min = min;
+	this.max = max;
     }
 
     guess() {
+    var i;
+	i = (this.max - this.min)/2;
+	i = i + this.min;
+    this.center = Math.round (i);
+	return this.center;
+
 
     }
 
     lower() {
-
+    this.max = this.center;
     }
 
     greater() {
-
+	this.min = this.center;
     }
 }
 
 module.exports = GuessingGame;
+
